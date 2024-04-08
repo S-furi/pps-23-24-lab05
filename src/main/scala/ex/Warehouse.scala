59bef317f97ec311f6d54a024dccb9a6d213014f
S-furi <stefano.furi@studio.unibo.it>
2024-04-08 11:34:44 +0200

    feat(ex2): complete task 2

--- src/main/scala/ex/Warehouse.scala
+++ src/main/scala/ex/Warehouse.scala
@@ -2,13 +2,16 @@ package ex
 
 import util.Optionals.Optional
 import util.Sequences.*
+import util.Sequences.Sequence.*
+
 trait Item:
   def code: Int
   def name: String
   def tags: Sequence[String]
 
 object Item:
-  def apply(code: Int, name: String, tags: Sequence[String] = Sequence.empty): Item = ???
+  def apply(code: Int, name: String, tags: String*): Item = ItemImpl(code, name, tags.foldLeft(Nil())((curr, acc) => Cons(acc, curr)))
+  private case class ItemImpl(code: Int, name: String, tags: Sequence[String]) extends Item
 
 /**
  * A warehouse is a place where items are stored.
@@ -45,14 +48,27 @@ trait Warehouse:
 end Warehouse
 
 object Warehouse:
-  def apply(): Warehouse = ???
+  def apply(): Warehouse = new WarehouseImpl()
+
+  private class WarehouseImpl extends Warehouse:
+    private var items: Sequence[Item] = Sequence.empty
+
+    override def store(item: Item): Unit = items = Cons(item, items)
+
+    override def searchItems(tag: String): Sequence[Item] = items.filter(_.tags.contains(tag))
+
+    override def retrieve(code: Int): Optional[Item] = items.find(_.code == code)
+
+    override def remove(item: Item): Unit = items = items.filter(_ != item)
+
+    override def contains(itemCode: Int): Boolean = !items.find(_.code == itemCode).isEmpty
 
 @main def mainWarehouse(): Unit =
   val warehouse = Warehouse()
 
-  val dellXps = Item(33, "Dell XPS 15", Sequence("notebook"))
-  val dellInspiron = Item(34, "Dell Inspiron 13", Sequence("notebook"))
-  val xiaomiMoped = Item(35, "Xiaomi S1", Sequence("moped", "mobility"))
+  val dellXps = Item(33, "Dell XPS 15", "notebook")
+  val dellInspiron = Item(34, "Dell Inspiron 13", "notebook")
+  val xiaomiMoped = Item(35, "Xiaomi S1", "moped", "mobility")
 
   warehouse.contains(dellXps.code) // false
   warehouse.store(dellXps) // side effect, add dell xps to the warehouse
---
+++ src/test/scala/ex/WarehouseTest.scala
@@ -0,0 +1,54 @@
+package ex
+
+import org.junit.jupiter.api.{BeforeEach, Test}
+import org.junit.jupiter.api.Assertions.{assertEquals, assertNotNull, assertTrue, assertFalse}
+import util.Sequences.*
+import util.Sequences.Sequence.*
+
+class WarehouseTest:
+  private val i = Item(code = 1, name = "test", "a", "b", "c")
+  private var w = Warehouse()
+
+  @BeforeEach def setUp(): Unit = {
+    w = Warehouse()
+    w.store(i)
+  }
+
+  @Test def testItemCreation(): Unit = {
+    assertEquals(1, i.code)
+    assertEquals("test", i.name)
+    assertEquals(Sequence("c", "b", "a"), i.tags)
+  }
+
+  @Test def testWarehouseContains(): Unit = {
+    assertFalse(w.contains(-1))
+    assertTrue(w.contains(i.code))
+  }
+
+  @Test def testWarehouseCanStoreItems(): Unit = {
+    val i2 = Item(code = 2, "test2", "foo")
+    w.store(i2)
+    assertTrue(w.contains(i.code))
+    assertTrue(w.contains(i2.code))
+  }
+
+  @Test def testWarehouseCanSearchItem(): Unit = {
+    val i2 = Item(code = 2, "test2", "a")
+    w.store(i2)
+    val items = w.searchItems("a")
+    assertTrue(items.contains(i))
+    assertTrue(items.contains(i2))
+  }
+
+  @Test def testWarehouseCanRetrieveItem(): Unit = {
+    val item = w.retrieve(i.code)
+    assertFalse(item.isEmpty)
+    assertEquals(i, item.orElse(Item(code = -1, name = "error")))
+  }
+
+  @Test def testWarehouseCanRemoveItem(): Unit = {
+    val i2 = Item(code = 2, "test2", "a")
+    w.store(i2)
+    w.remove(i2)
+    assertTrue(w.retrieve(i2.code).isEmpty)
+  }
